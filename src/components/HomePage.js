94458f947cb02928cc907b0348f8872a8ff1c452
marcos.asdes <marco.engc@poli.ufrj.br>
2022-05-20 22:11:13 -0300

    added css -> HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
 
+
+
 export default function HomePage(){
     const [courses, setCourses] = useState("");
 
@@ -20,8 +22,8 @@ export default function HomePage(){
                     courses.map((course) => {
                         const {_id, title} = course
                         return (
-                            <Link to={`/course/${title}`} key={_id}>
-                            <div>{title}</div>
+                            <Link to={`/course/${title}`} key={_id} style={{ textDecoration: 'none' }}>
+                            <div className="course">{title}</div>
                             </Link>
                         )
                     })
@@ -34,27 +36,50 @@ export default function HomePage(){
 // configurar css
 const Home = styled.main`
     & {
+        background-color: #575757;
+        box-sizing: border-box;
+        width: 100vw;
+        height: 100vh;
+        overflow-y: scroll;
         padding-top: 70px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        font-family: 'Roboto', sans-serif;
     }
-
-    div {
-        width: 300px;
+    .title {
+        display: flex;
+        align-items: center;
+        color: #02cf2b;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 42px;
+        line-height: 45px;
+        letter-spacing: 0.01em;
+        margin: 40px 0 40px 0;
+    }
+    .courses {
+        display: flex;
+        flex-direction: column;
+        width: 100vw;
+        align-items: center;
+    }
+    .course {
+        display: flex;
+        width: calc(100vw - 40px);
         height: 65px;
-        background: #FFFFFF;
+        background: #02cf2b;
         box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
         border-radius: 5px;
         align-items: center;
         justify-content: space-between;
-        padding: 0 22px 0 15px;
+        padding: 0 15px 0 15px;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 20px;
+        letter-spacing: 0.01em;
+        color: #F5F5F5;
+        margin-bottom: 20px;
     }
-
-    .title {
-
-    }
-
-    .courses {
-        display: flex;
-        margin: 0 0 25px 0;
-    }
-
 `
